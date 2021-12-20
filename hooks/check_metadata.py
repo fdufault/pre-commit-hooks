51c87e1fbe33207dfeaae56fa6eb08f6941619bd
fdufault <francis.dufault@hotmail.com>
2021-12-20 12:50:52 -0500

    Update check_metadata.py

--- hooks/check_metadata.py
+++ hooks/check_metadata.py
@@ -2,19 +2,27 @@ from typing import Optional
 from typing import Sequence
 import argparse
 
-def check_metadata(content, field_list, filename):
-    
+
+def get_checklist(fields):
     checklist = {}
+    for field in fields.split(","):
+        checklist[field] = False
+    return checklist
+
+
+def check_metadata(content, fields, filename):
+    status = 0
+    checklist = get_checklist(fields)
     for line in content:
         if line.startswith("#"):
-            for field in field_list:
+            for field in fields:
                 if field in line:
                     checklist[field] = True
-                    
-    if len(field_list) != len(checklist):
-        return 1
-            
-    return 0
+    for field in checklist:
+        if not checklist[field]:
+            print(filename, "missing field:", field)
+            status = 1
+    return status 
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -28,17 +36,13 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         help='List of metadata fields to check.',
     )
     args = parser.parse_args(argv)
-    
     if args.fields is not None:
-        field_list = args.fields.split(",")
+        fields = args.fields.split(",")
     else:
         return 0
-
     retv = 0
-
     for filename in args.filenames:
         with open(filename, 'r') as f:
             content = f.readlines()
-        retv |= check_metadata(content, field_list, filename)
-
+        retv |= check_metadata(content, fields, filename)
     return retv
